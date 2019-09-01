726f6e975576ebcd67b7cbbf937719a453bd9f8f
chujian0325 <653510854@qq.com>
2019-09-01 20:30:31 +0800

    登录页面完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -8,22 +8,43 @@
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button class="login-btn" type="primary" @click.prevent="handleLogin()">登录</el-button>
     </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       formdata: {
-        username: '',
-        password: ''
+        username: "",
+        password: ""
       }
+    };
+  },
+  methods: {
+    handleLogin() {
+      this.$http
+        .post("login", this.formdata)
+        .then(res => {
+          // console.log(res);
+          const {
+            data: {
+              meta: { status, msg }
+            }
+          } = res;
+          if (status === 200) {
+          } else {
+            this.$message.error(msg);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
-}
+};
 </script>
 
 <style>
