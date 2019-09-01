eba8d2f58c1dd32db208caf9301089945736c6ce
chujian0325 <653510854@qq.com>
2019-09-01 21:22:33 +0800

    home主页头部样式调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,23 @@
 <template>
-  <div></div>
+  <el-container class="container">
+    <el-header>
+      <el-col>
+        <el-col :span="4">
+          <img src="../assets/logo.png" alt="">
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="logout">退出</a>
+        </el-col>
+      </el-col>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -7,4 +25,23 @@ export default {}
 </script>
 
 <style>
+.container {
+  height: 100%;
+  background-color: red;
+}
+.aside {
+  background-color: yellow;
+}
+.main {
+  background-color: green;
+}
+.middle {
+  text-align: center;
+  line-height: 60px;
+  color: #fff;
+}
+.logout {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
