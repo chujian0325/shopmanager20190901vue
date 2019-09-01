8a7ee2d9d2b3bae31e3736a555a8dcac14176a6e
chujian0325 <653510854@qq.com>
2019-09-01 21:47:42 +0800

    home主页侧边栏完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,7 +3,7 @@
     <el-header>
       <el-col>
         <el-col :span="4">
-          <img src="../assets/logo.png" alt="">
+          <img src="../assets/logo.png" alt />
         </el-col>
         <el-col :span="19" class="middle">
           <h2>电商后台管理系统</h2>
@@ -14,7 +14,63 @@
       </el-col>
     </el-header>
     <el-container>
-      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-aside width="200px" class="aside">
+        <el-menu :unique-opened="true" :router="true" default-active="2" class="el-menu-vertical-demo">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>用户列表
+            </el-menu-item>
+          </el-submenu>
+           <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>角色列表
+            </el-menu-item><el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>权限列表
+            </el-menu-item>
+          </el-submenu>
+           <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>商品列表
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>分类参数
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>商品分类
+            </el-menu-item>
+          </el-submenu>
+           <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>订单列表
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>数据报表
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
